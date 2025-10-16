e7e26825ec22f22cfafd0f3dc3bb6444761f923b
Shweta <shwetaratnapure139@gmail.com>
2025-10-17 00:37:06 +0530

    commited vite

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Jaruratcare_ReactTask/' // तुमच्या GitHub repo नावाशी match करा
+  base: '/Jaruratcare_ReactTask/' 
 })
